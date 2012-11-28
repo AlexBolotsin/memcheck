94cda196f2eccc59f039cf7fe980eb3725ed596e
alexb.hpp <alexbolotsin@gmail.com>
2012-11-28 23:38:19 +0300

    Use preprocessor to get function name

--- allocation_hooks.cpp
+++ allocation_hooks.cpp
@@ -2,22 +2,12 @@
 #include <iostream>
 #include <cstring>
 #include <map>
-#include <typeinfo>
-
-const char* ALLOC_STRINGS[] = {
-  "operator new",
-  "operator new[]",
-  "malloc",
-  "calloc",
-  "realloc"
-};
-enum AllocType { NEW, NEW_WITH_BRACKETS, MALLOC, CALLOC, REALLOC };
 
 struct PtrInfo{
   const char* file;
   int line;
   int size;
-  AllocType type;
+  const char* func_name;
 };
 
 std::map<void*, PtrInfo> ptr_map;
@@ -30,15 +20,30 @@ void erase_ptr(void* ptr) {
   ptr_map.erase(ptr);
 }
 
+void check_map() {
+  if(ptr_map.empty()) return;
+  std::cout << "Memory leak summary:" << std::endl;
+  std::map<void*, PtrInfo>::const_iterator iter = ptr_map.begin();
+  unsigned int leaked_bytes = 0;
+  for ( ; iter != ptr_map.end(); ++iter) {
+    std::cout << "\t" << iter->second.func_name << " (" << std::hex
+	      << iter->first << std::dec << ") missed " << iter->second.size
+	      << " bytes in " << iter->second.file << ":" << iter->second.line <<  std::endl;
+    leaked_bytes += iter->second.size;
+    free(iter->first);
+  }
+  std::cout << "Total leaked " << leaked_bytes << " bytes" << std::endl;
+}
+
 void* operator new(size_t size, const char* file, int line) {
   void* ptr = malloc(size);
-  add_ptr(ptr, {file, line, size, NEW});
+  add_ptr(ptr, {file, line, size, __FUNCTION__});
   return ptr;
 }
 
 void* operator new[](size_t size, const char* file, int line) {
   void* ptr = malloc(size);
-  add_ptr(ptr, {file, line, size, NEW_WITH_BRACKETS});
+  add_ptr(ptr, {file, line, size, __FUNCTION__});
   ptr;
 }
 
@@ -54,37 +59,24 @@ void operator delete[](void* ptr) {
 
 void* malloc(size_t size, const char* file, int line) {
   void* ptr = malloc(size);
-  ptr_map[ptr] = {file, line, size, MALLOC};
+  ptr_map[ptr] = {file, line, size, __FUNCTION__};
   return ptr;
 }
+
 void* calloc(size_t num, size_t size, const char* file, int line) {
   void* ptr = calloc(num, size);  
-  add_ptr(ptr, {file, line, size, CALLOC});
+  add_ptr(ptr, {file, line, size, __FUNCTION__});
   return ptr;
 }
+
 void* realloc(void* source, size_t size, const char* file, int line) {
   void* ptr = realloc(source, size);
   erase_ptr(source);
-  add_ptr(ptr, {file, line, size, REALLOC});
+  add_ptr(ptr, {file, line, size, __FUNCTION__});
   return ptr;
 }
+
 void free(void* ptr, const char* file, int line) {
   erase_ptr(ptr);
   free(ptr);
-}
-
-
-void check_map() {
-  if(ptr_map.empty()) return;
-  std::cout << "Memory leak summary:" << std::endl;
-  std::map<void*, PtrInfo>::const_iterator iter = ptr_map.begin();
-  unsigned int leaked_bytes = 0;
-  for ( ; iter != ptr_map.end(); ++iter) {
-    std::cout << "\t" << ALLOC_STRINGS[iter->second.type] << " (" << std::hex
-	      << iter->first << std::dec << ") missed " << iter->second.size
-	      << " bytes in " << iter->second.file << ":" << iter->second.line <<  std::endl;
-    leaked_bytes += iter->second.size;
-    free(iter->first);
-  }
-  std::cout << "Total leaked " << leaked_bytes << " bytes" << std::endl;
 }
