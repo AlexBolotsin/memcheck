528e37cf817d75a8ae246f688d46dc42dea0eac8
alexb.hpp <alexbolotsin@gmail.com>
2012-11-28 21:20:16 +0300

    Fixed malloc, calloc and realloc

--- allocation_hooks.cpp
+++ allocation_hooks.cpp
@@ -10,48 +10,58 @@ struct PtrInfo{
 
 std::map<void*, PtrInfo> ptr_map;
 
+void add_ptr(void* ptr, const PtrInfo& info) {
+  ptr_map[ptr] = info;  
+}
+
+void erase_ptr(void* ptr) {
+  ptr_map.erase(ptr);
+}
+
 void* operator new(size_t size, const char* file, int line) {
   void* ptr = malloc(size);
   //std::cout << std::hex << ptr << std::dec << " " << file << " " << line << std::endl;
-  ptr_map[ptr] = {file, line};
+  add_ptr(ptr, {file, line});
   return ptr;
 }
 
 void* operator new[](size_t size, const char* file, int line) {
   void* ptr = malloc(size);
   //std::cout << std::hex << ptr << std::dec << " " << file << " " << line << std::endl;
-  ptr_map[ptr] = {file, line};
-  return ptr;
+  add_ptr(ptr, {file, line});
+  ptr;
 }
 
 void operator delete(void* ptr) {
   //std::cout << std::hex << ptr << std::endl;
-  ptr_map.erase(ptr);
+  erase_ptr(ptr);
   free(ptr);
 }
 
 void operator delete[](void* ptr) {
   //std::cout << std::hex << ptr << std::endl;
-  ptr_map.erase(ptr);
+  erase_ptr(ptr);
   free(ptr);
 }
 
 void* malloc(size_t size, const char* file, int line) {
   void* ptr = malloc(size);
   ptr_map[ptr] = {file, line};
+  return ptr;
 }
 void* calloc(size_t num, size_t size, const char* file, int line) {
-  void* ptr = malloc(num*size);
-  ptr_map[ptr] = {file, line};
+  void* ptr = calloc(num, size);  
+  add_ptr(ptr, {file, line});
+  return ptr;
 }
 void* realloc(void* source, size_t size, const char* file, int line) {
-  void* ptr = malloc(size);
-  memcpy(ptr, source, size);
-  ptr_map[ptr] = {file, line};
+  void* ptr = realloc(source, size);
+  erase_ptr(source);
+  add_ptr(ptr, {file, line});
   return ptr;
 }
 void free(void* ptr, const char* file, int line) {
-  ptr_map.erase(ptr);
+  erase_ptr(ptr);
   free(ptr);
 }
 
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char** argv) {
   num = new int[1];
   void* chunk = malloc(sizeof(int));
   chunk = calloc(1, sizeof(int));
-  chunk = realloc(num, sizeof(int));
+  chunk = realloc(chunk, 2*sizeof(num));
+  chunk = realloc(chunk, 2*sizeof(num));
   check_map();
 }
