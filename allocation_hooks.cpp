8e64584cebd98a5fa75fb1e06bedc17a94e36e7f
alexb.hpp <alexbolotsin@gmail.com>
2012-11-28 21:59:59 +0300

    Modify output

--- allocation_hooks.cpp
+++ allocation_hooks.cpp
@@ -2,10 +2,22 @@
 #include <iostream>
 #include <cstring>
 #include <map>
+#include <typeinfo>
+
+const char* ALLOC_STRINGS[] = {
+  "operator new",
+  "operator new[]",
+  "malloc",
+  "calloc",
+  "realloc"
+};
+enum AllocType { NEW, NEW_WITH_BRACKETS, MALLOC, CALLOC, REALLOC };
 
 struct PtrInfo{
   const char* file;
   int line;
+  int size;
+  AllocType type;
 };
 
 std::map<void*, PtrInfo> ptr_map;
@@ -20,44 +32,40 @@ void erase_ptr(void* ptr) {
 
 void* operator new(size_t size, const char* file, int line) {
   void* ptr = malloc(size);
-  //std::cout << std::hex << ptr << std::dec << " " << file << " " << line << std::endl;
-  add_ptr(ptr, {file, line});
+  add_ptr(ptr, {file, line, size, NEW});
   return ptr;
 }
 
 void* operator new[](size_t size, const char* file, int line) {
   void* ptr = malloc(size);
-  //std::cout << std::hex << ptr << std::dec << " " << file << " " << line << std::endl;
-  add_ptr(ptr, {file, line});
+  add_ptr(ptr, {file, line, size, NEW_WITH_BRACKETS});
   ptr;
 }
 
 void operator delete(void* ptr) {
-  //std::cout << std::hex << ptr << std::endl;
   erase_ptr(ptr);
   free(ptr);
 }
 
 void operator delete[](void* ptr) {
-  //std::cout << std::hex << ptr << std::endl;
   erase_ptr(ptr);
   free(ptr);
 }
 
 void* malloc(size_t size, const char* file, int line) {
   void* ptr = malloc(size);
-  ptr_map[ptr] = {file, line};
+  ptr_map[ptr] = {file, line, size, MALLOC};
   return ptr;
 }
 void* calloc(size_t num, size_t size, const char* file, int line) {
   void* ptr = calloc(num, size);  
-  add_ptr(ptr, {file, line});
+  add_ptr(ptr, {file, line, size, CALLOC});
   return ptr;
 }
 void* realloc(void* source, size_t size, const char* file, int line) {
   void* ptr = realloc(source, size);
   erase_ptr(source);
-  add_ptr(ptr, {file, line});
+  add_ptr(ptr, {file, line, size, REALLOC});
   return ptr;
 }
 void free(void* ptr, const char* file, int line) {
@@ -68,10 +76,15 @@ void free(void* ptr, const char* file, int line) {
 
 void check_map() {
   if(ptr_map.empty()) return;
+  std::cout << "Memory leak summary:" << std::endl;
   std::map<void*, PtrInfo>::const_iterator iter = ptr_map.begin();
+  unsigned int leaked_bytes = 0;
   for ( ; iter != ptr_map.end(); ++iter) {
-    std::cout << "MEMORY LEAK! " << std::hex << iter->first << std::dec << " "
-	      << iter->second.file << " " << iter->second.line << std::endl;
+    std::cout << "\t" << ALLOC_STRINGS[iter->second.type] << " (" << std::hex
+	      << iter->first << std::dec << ") missed " << iter->second.size
+	      << " bytes in " << iter->second.file << ":" << iter->second.line <<  std::endl;
+    leaked_bytes += iter->second.size;
     free(iter->first);
   }
+  std::cout << "Total leaked " << leaked_bytes << " bytes" << std::endl;
 }
